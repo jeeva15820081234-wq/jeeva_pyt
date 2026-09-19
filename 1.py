8df333750593d14b4af32d32670ee515c2e7cb07
jeeva15820081234-wq <jeeva15820081234@gmail.com>
2026-09-19 08:50:44 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+print(type(25))
+print(type(25.5))
+print(type('25'))
+age = 21
+height = 5.8
+name = 'Arun'
+age = 20
+price = 99.5
+name = 'Ravi'
+marks = 90
+print(type(marks))
+temperature = 36.5
+print(type(temperature))
+message = 'Welcome'
+print(type(message))
---
+++ 5.py
@@ -0,0 +1,25 @@
+print('Start\nMiddle\nEnd')
+print('First')
+print('Second')
+print('Third')
+x = 10
+print(x)
+x = 20
+print(x)
+number = 5
+print(number)
+number = 10
+print(number)
+print('A\nB\nC')
+name = 'Ravi'
+age = 20
+print(name, age)
+print(1)
+print(2)
+print(3)
+print(4)
+x = 5
+y = 10
+print(x, y)
+print('Start',2 + 3, 'End')
+print('Login\nDashboard\nLogout')
